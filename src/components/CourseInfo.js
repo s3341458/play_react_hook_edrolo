bace46c173a5efe8dcf7d610bedec33a33618a02
chengyu <chengyu0316@gmail.com>
2020-03-06 15:28:25 +1100

    task 3 able to manipulate state of components

--- src/components/CourseInfo.js
+++ src/components/CourseInfo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const CourseInfo = ({ title, caption, infoUrl }) => (
+const CourseInfo = ({ title, caption, infoUrl, clickTitle }) => (
   <div className="courseInfo">
-    <h3 className="courseTitle">{title}</h3>
+    <h3 className="courseTitle" onClick={clickTitle}>{title}</h3>
     <p>
       {caption}
       <a href="{infoUrl}">read more...</a>
--- src/components/Website.js
+++ src/components/Website.js
@@ -1,19 +1,45 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
-import CourseInfo from './CourseInfo'
+import CourseInfo from './CourseInfo';
 
 const Website = ({ courses }) => {
+  const [favorites, setFavorites] = useState(new Set());
+  const addToFavrite = course => () => {
+    let newFavorites = new Set(favorites);
+    newFavorites.add(course);
+    setFavorites(newFavorites);
+  };
+  const deleteFavrite = course => () => {
+    let newFavorites = new Set(favorites);
+    newFavorites.delete(course);
+    setFavorites(newFavorites);
+  };
   return (
     <div>
       <Header />
       <div>Hello and welcome to Edrolo's new website.</div>
       <div className="subjects">
         <h2>Subjects We Offer</h2>
-          {courses.map(course => <CourseInfo key={course.id} {...course}/>)}
+        {courses.map(course => (
+          <CourseInfo
+            key={course.id}
+            clickTitle={addToFavrite(course)}
+            {...course}
+          />
+        ))}
       </div>
       <div>
         <h2>Your Favorites</h2>
-        <ul></ul>
+        <ul>
+          {[...favorites].map(course => (
+            <li key={`favorites-${course.id}`}>
+              <p>
+                {course.title}{' '}
+                <button onClick={deleteFavrite(course)}>remove</button>
+              </p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
